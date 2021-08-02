46aad5110fe369bc6fafa2efca78c059698efc80
saranyadurairaju <saranya.or@gmail.com>
2021-08-02 16:33:58 -0500

    Updating the ReadMe file

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -45,7 +45,7 @@ INTO mentorship_eligibility
 FROM employees AS e
 JOIN dept_employees AS de
 ON (e.emp_no = de.emp_no)
-JOIN titles_new AS t
+JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01' AND 
 	   (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'))
